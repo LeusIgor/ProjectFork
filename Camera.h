e3cf9ab82b17398035016742dc3ce2aa216f2fbc
LeusIgor <161763264+LeusIgor@users.noreply.github.com>
2024-03-01 15:53:17 -0800

    Documentation comments added to Cellphone.h and Camera.h

--- Camera.h
+++ Camera.h
@@ -1,38 +1,67 @@
 #ifndef PROJECT_FORK_CAMERA_H
 #define PROJECT_FORK_CAMERA_H
 
+/**
+ * Represents a camera with the following functionalities:
+ * make photos and videos, along with sensor size, camera resolution, frame rate, and
+ * lens replaceability.
+ */
 class Camera {
 public:
-    Camera(); // default
-    Camera(int sensor_size, int resolution, int frame_rate, bool replaceable_lenses); //параметризованный конструктор для самостоятельного использования
-    Camera(int sensor_size, int resolution, int frame_rate); //параметризованный конструктор для использования в классах наследников
+    /** Default constructor. */
+    Camera();
+    /** Parameterized constructor for independent use. */
+    Camera(int sensor_size, int resolution, int frame_rate, bool replaceable_lenses);
+    /** Parameterized constructor for use in descendant classes. */
+    Camera(int sensor_size, int resolution, int frame_rate);
 
+    /** GETTER: Returns the sensor size of phone number. */
     int getSensorSize() const;
-
+    /** GETTER: Returns the camera resolution. */
     int getResolution() const;
-
+    /** GETTER: Returns the frame rate. */
     int getFrameRate() const;
-
+    /** GETTER: Checks if lenses are replaceable. */
     bool isReplaceableLenses() const;
 
+    /** SETTER: Sets the sensor size of phone number. */
     void setSensorSize(int sensorSize);
-
+    /** SETTER: Sets the camera resolution. */
     void setResolution(int resolution);
-
+    /** SETTER: Sets the frame rate. */
     void setFrameRate(int frameRate);
-
+    /** SETTER: Sets if lenses are replaceable. */
     void setReplaceableLenses(bool replaceableLenses);
 
+    /**
+ * Provide an answer to the question: "is the gadget shoots photos"?
+ * @param empty
+ * @return the answer
+ */
     void make_photo();
+    /**
+  * Provide an answer to the question: "is the gadget record videos"?
+  * @param empty
+  * @return the answer
+  */
     void make_video();
 
 protected:
+    /** Sensor size of the camera. */
     int sensor_size_;
+    /** Resolution of the camera. */
     int resolution_;
+    /** Frame rate for video recording. */
     int frame_rate_;
 
 private:
+    /** Indicate if lenses can be replaced. */
     bool replaceable_lenses_;
+    /**
+ * Provide an answer to the question: "is the gadget has physical adjusters"?
+ * @param empty
+ * @return the answer
+ */
     void physical_adjusters();
 };
 
--- Cellphone.h
+++ Cellphone.h
@@ -1,41 +1,57 @@
-#include <string>
-
 #ifndef PROJECT_FORK_CELLPHONE_H
 #define PROJECT_FORK_CELLPHONE_H
 
+#include <string>
+
+/**
+ * Represents a cell phone with the following functionalities:
+ * make calls, manage phone number, SIM card, and physical keyboard.
+ */
 class Cellphone {
 public:
-    Cellphone();// default
+    /** Default constructor. */
+    Cellphone();
+    /** Parameterized constructor for independent use. */
     Cellphone(std::string phone_number, bool physical_sim_card, bool physical_keyboard);
+    /** Parameterized constructor for use in descendant classes. */
     Cellphone(std::string phone_number, bool physical_sim_card);
 
+    /**
+     * Provide an answer to the question: "is the gadget make phone calls"?
+     * @param empty
+     * @return the answer
+     */
     bool make_calls();
 
+    /** GETTER: Checks the presence of phone number. */
     const std::string &getPhoneNumber() const;
-
+    /** GETTER: Checks the presence of physical sim card. */
     bool isPhysicalSimCard() const;
-
+    /** GETTER: Checks the presence of physical keyboard. */
     bool isPhysicalKeyboard() const;
 
+    /** SETTER: Sets the phone number. */
     void setPhoneNumber(const std::string &phoneNumber);
-
+    /** SETTER: Sets the presence of physical sim card. */
     void setPhysicalSimCard(bool physicalSimCard);
-
+    /** SETTER: Sets the presence of physical keyboard. */
     void setPhysicalKeyboard(bool physicalKeyboard);
 
 protected:
+    /** indicates the presence of phone number. */
     std::string phone_number_;
+    /** Indicates the presence of physical sim card. */
     bool physical_sim_card_;
 
 private:
+    /** Indicates the presence of physical keyboard. */
     bool physical_keyboard_;
     /**
-     * gives an answer for question is phone indestructible?
+     * Provide an answer to the question: "is the gadget indestructible"?
      * @param empty
      * @return the answer
      */
     std::string indestructible();
 };
-
 
 #endif //PROJECT_FORK_CELLPHONE_H

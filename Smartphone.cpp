a5778c97e05fde2071a587833dcd267ef6ad4d4b
LeusIgor <161763264+LeusIgor@users.noreply.github.com>
2024-03-01 23:55:10 -0800

    Declared essential elements in Smartphone.h

--- Smartphone.cpp
+++ Smartphone.cpp
@@ -1,2 +1,18 @@
 
 #include "Smartphone.h"
+
+bool Smartphone::isWiFi() const {
+    return WiFi_;
+}
+
+bool Smartphone::isDigitalFingerprint() const {
+    return Digital_fingerprint_;
+}
+
+void Smartphone::setWiFi(bool wiFi) {
+    WiFi_ = wiFi;
+}
+
+void Smartphone::setDigitalFingerprint(bool digitalFingerprint) {
+    Digital_fingerprint_ = digitalFingerprint;
+}
